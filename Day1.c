00ed92c246f99dd76a0f5031ab0605300921eddf
Priyanshi Sharma <25116cn217@glbitm.ac.in>
2026-05-22 23:11:22 +0530

    Updated day1.c

--- Day1.c
+++ Day1.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 int main() {
-    printf("Hello, I'm Priyanshi Sharma\n");
+    int num , sum =0;
+    printf("Enter a number N: ");
+    scanf("%d", &num);
+    while(num!=0){
+        sum+=num;
+        num--;
+    }
+    printf("The sum of first N natural numbers is : %d ", sum);
+
     return 0;
 }
