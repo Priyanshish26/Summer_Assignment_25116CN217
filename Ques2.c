8ce461a2986c3b33991d9ecf359911727f793774
Priyanshi Sharma <25116cn217@glbitm.ac.in>
2026-05-29 21:02:39 +0530

    Updated code

--- Ques2.c
+++ Ques2.c
@@ -3,7 +3,9 @@
 // Write a program to Reverse a number. 
 
 int main() {
+
     int num, remainder, rev=0;
+    
     printf("Enter a number to reverse : ");
     scanf("%d", &num );
 
@@ -12,7 +14,8 @@ int main() {
         rev = rev*10 + remainder ;
         num = num/10;
     }
-    
+
     printf("Reversed number is %d", rev);
+
     return 0 ;
 }
