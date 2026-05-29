617b6719522930e76b35128d0c49577b319ae4de
Priyanshi Sharma <25116cn217@glbitm.ac.in>
2026-05-29 20:57:46 +0530

    Updated

--- Ques2.c
+++ Ques2.c
@@ -6,11 +6,13 @@ int main() {
     int num, remainder, rev=0;
     printf("Enter a number to reverse : ");
     scanf("%d", &num );
+
     while(num!=0){
         remainder = num%10;
         rev = rev*10 + remainder ;
         num = num/10;
     }
+    
     printf("Reversed number is %d", rev);
     return 0 ;
 }
