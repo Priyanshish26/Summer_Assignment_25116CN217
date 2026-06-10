fcda6c8d7e37ad3052907d137053cc6cb0e2d9c1
Priyanshi Sharma <25116cn217@glbitm.ac.in>
2026-06-10 18:18:15 +0530

    Added Day2 folder

---
+++ Day02/Ques6.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Write a program to Reverse a number. 
+
+int main() {
+
+    int num, remainder, rev=0;
+
+    printf("Enter a number to reverse : ");
+    scanf("%d", &num );
+
+    while(num!=0){
+        remainder = num%10;
+        rev = rev*10 + remainder ;
+        num = num/10;
+    }
+
+    printf("Reversed number is %d", rev);
+
+    return 0 ;
+}
